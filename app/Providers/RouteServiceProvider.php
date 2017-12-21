87b3e64bc4ac54f72e035fa95635788b5e81b577
shohan494 <shohan12224943@gmail.com>
2017-12-22 02:38:55 +0600

    no prefix needed because web and api are needed to stay same

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -65,8 +65,8 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapApiRoutes()
     {
-        Route::prefix('api')
-             ->middleware('api')
+        // no prefix needed because web and api are needed to stay same
+        Route::middleware('api')
              ->namespace($this->namespace)
              ->group(base_path('routes/api.php'));
     }
