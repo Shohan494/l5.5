c5f02362da24fa260bbe83794214b137f3dc0566
shohan494 <shohan12224943@gmail.com>
2017-12-23 00:47:42 +0600

    user table migration modified

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
+            $table->string('verified');
+            $table->string('verification_token');
             $table->timestamps();
         });
     }
