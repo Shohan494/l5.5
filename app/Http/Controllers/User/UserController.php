bc8a548b17a5a49b3ee4242291cf3f9cc901d284
shohan494 <shohan12224943@gmail.com>
2017-12-28 23:19:36 +0600

    show method is user controller modified

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -40,7 +40,8 @@ class UserController extends Controller
      */
     public function show($id)
     {
-        $user = User::find($id);
+        $user = User::findOrFail($id);
+        return response()->json(['data' => $user], 200);
     }
 
     /**
