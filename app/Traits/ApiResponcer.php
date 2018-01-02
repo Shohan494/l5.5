4f44e43ab1439f4cef6ed67847144831f5e9c982
shohan494 <shohan12224943@gmail.com>
2018-01-03 03:37:55 +0600

    generalizing response methods using traits

--- app/Traits/ApiResponcer.php
+++ app/Traits/ApiResponcer.php
@@ -1,3 +1,30 @@
 <?php
 
 namespace App\Traits;
+
+use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+trait ApiResponcer
+{
+		private function succesResponce($data, $code)
+		{
+				return response()->json($data, $code);
+		}
+
+		protected function errorResponce($message, $code)
+		{
+				return response()->json(['error' => $message, 'code' => $code], $code);
+		}
+
+		protected function showAll(Collection $collection, $code = 200)
+		{
+				return $this->succesResponce(['data' => $collection], $code);
+		}
+
+		protected function showOne(Model $model, $code = 200)
+		{
+				return $this->succesResponce(['data' => $model], $code);
+		}
+
+}
