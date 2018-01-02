4d26dc7d15592145b6b525e2ef780a16bfb35f4e
shohan494 <shohan12224943@gmail.com>
2018-01-03 00:31:11 +0600

    mutuators and accessors of models

--- app/User.php
+++ app/User.php
@@ -45,6 +45,27 @@ class User extends Authenticatable
         'verification_token',
     ];
 
+    public function setNameAttribute($name)
+    {
+        $this->attributes['name'] = strtolower($name);
+    }
+
+    public function getNameAttribute($name)
+    {
+        return ucwords($name);
+    }
+
+    public function setEmailAttribute($email)
+    {
+        $this->attributes['email'] = $email;
+    }
+
+    public function getEmailAttribute($email)
+    {
+        return ucwords($email);
+    }
+
+
     public function isVerified()
     {
         return $this->verified == User::VERIFIED_USER;
