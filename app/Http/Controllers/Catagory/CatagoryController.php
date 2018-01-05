f68a7f11cec59f882ee6290b8087e1c58d3394cc
shohan494 <shohan12224943@gmail.com>
2018-01-06 01:49:10 +0600

    php artisan make:controller Catagory/CatagoryController -r -m Catagory

--- app/Http/Controllers/Catagory/CatagoryController.php
+++ app/Http/Controllers/Catagory/CatagoryController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers\Catagory;
 
+use App\Catagory;
 use Illuminate\Http\Request;
-use App\Http\Controllers\ApiController;
+use App\Http\Controllers\Controller;
 
-class CatagoryController extends ApiController
+class CatagoryController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -41,10 +42,10 @@ class CatagoryController extends ApiController
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Catagory  $catagory
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Catagory $catagory)
     {
         //
     }
@@ -52,10 +53,10 @@ class CatagoryController extends ApiController
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Catagory  $catagory
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Catagory $catagory)
     {
         //
     }
@@ -64,10 +65,10 @@ class CatagoryController extends ApiController
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \App\Catagory  $catagory
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Catagory $catagory)
     {
         //
     }
@@ -75,10 +76,10 @@ class CatagoryController extends ApiController
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  \App\Catagory  $catagory
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Catagory $catagory)
     {
         //
     }
